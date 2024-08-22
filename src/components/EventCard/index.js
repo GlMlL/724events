e634d44c84689cf62382ede75c574972854c7ba2
Mariame <mariame.dibaga@gmail.com>
2024-08-22 12:21:10 +0000

    Filtrage des events

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,34 +1,24 @@
 import PropTypes from "prop-types";
 import { getMonth } from "../../helpers/Date";
+
 import "./style.scss";
 
-// Fonction utilitaire pour obtenir le dernier événement en date
-const getLastEvent = (events) => {
-  if (!events || events.length === 0) return null;
-  return events.sort((a, b) => new Date(b.date) - new Date(a.date))[0];
-};
-
 const EventCard = ({
-  events, // Modifié pour accepter une liste d'événements
+  imageSrc,
   imageAlt,
+  date = new Date(),
+  title,
+  label,
   small = false,
   ...props
-}) => {
-  // Obtenez le dernier événement en date
-  const latestEvent = getLastEvent(events);
-
-  if (!latestEvent) return null; // Ne rien afficher si aucun événement n'est disponible
-
-  const { imageSrc, date, title, label } = latestEvent;
-
-  return (
+}) => (
     <div
       data-testid="card-testid"
       className={`EventCard${small ? " EventCard--small" : ""}`}
       {...props}
     >
       <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt || "image"} />
+        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
         <div className="EventCard__label">{label}</div>
       </div>
       <div className="EventCard__descriptionContainer">
@@ -37,23 +27,19 @@ const EventCard = ({
       </div>
     </div>
   );
-};
 
 EventCard.propTypes = {
-  events: PropTypes.arrayOf(PropTypes.shape({
-    imageSrc: PropTypes.string.isRequired,
-    imageAlt: PropTypes.string,
-    date: PropTypes.instanceOf(Date).isRequired,
-    title: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-  })).isRequired,
+  imageSrc: PropTypes.string.isRequired,
   imageAlt: PropTypes.string,
+  date: PropTypes.instanceOf(Date).isRequired,
+  title: PropTypes.string.isRequired,
   small: PropTypes.bool,
+  label: PropTypes.string.isRequired,
 };
 
 EventCard.defaultProps = {
   imageAlt: "image",
   small: false,
-};
+}
 
 export default EventCard;
